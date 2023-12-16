f99e5e7eddf499e01381567dc643a05198477591
maurice diong <mdiong00@gmail.com>
2023-12-15 21:45:06 -0500

    the filter component is added

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,29 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedName, setSelectedName] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function onSearchChange(event){
+    console.log(event.target.value)
+    setSelectedName(event.target.value)
+  }
+  
+  let itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  if (onSearchChange) { itemsToDisplay = ( items.filter((item) => item.name.includes(selectedName)))
+  }
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
